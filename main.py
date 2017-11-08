d62b3b0c3013e3a01db4632e0681a91d02b21c9c
PhilippeW <philippe.weingertner@gmail.com>
2017-11-08 18:54:47 +0100

    remove pass

--- main.py
+++ main.py
@@ -129,7 +129,6 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for epoch in range(epochs):
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: image, correct_label: label, keep_prob: 0.8})
-    pass
 tests.test_train_nn(train_nn)
 
 
