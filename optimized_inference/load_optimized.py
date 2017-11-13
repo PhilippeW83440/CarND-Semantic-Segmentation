b77fc6466f896825c695a271f30ef667de2c76fd
PhilippeW <philippe.weingertner@gmail.com>
2017-11-13 19:02:15 +0100

    optimized_inference: on going work

---
+++ optimized_inference/load_optimized.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+def load_graph(graph_file, use_xla=False):
+    jit_level = 0
+    config = tf.ConfigProto()
+    if use_xla:
+        jit_level = tf.OptimizerOptions.ON_1
+        config.graph_options.optimizer_options.global_jit_level = jit_level
+
+    with tf.Session(graph=tf.Graph(), config=config) as sess:
+        gd = tf.GraphDef()
+        with tf.gfile.Open(graph_file, 'rb') as f:
+            data = f.read()
+            gd.ParseFromString(data)
+        tf.import_graph_def(gd, name='')
+        ops = sess.graph.get_operations()
+        n_ops = len(ops)
+        return sess.graph, ops
+
+
+sess, optimized_ops = load_graph('optimized_graph.pb')
+print(len(optimized_ops)) # 200
